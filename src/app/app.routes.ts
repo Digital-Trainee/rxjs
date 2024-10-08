2a63e578b3373c7e715c7b29dc77f45a0380e7db
Digital-Trainee <digitaltraineepune@gmail.com>
2024-10-08 18:47:14 +0530

    Create of operator and of operator use

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { IntervalComponent } from './interval/interval.component';
 import { Routes } from '@angular/router';
 import { TimerComponent } from './timer/timer.component';
+import { OfComponent } from './of/of.component';
 
 export const routes: Routes = [
   {path:'',component: IntervalComponent },
-  {path:'timer',component:TimerComponent}
+  {path:'timer',component:TimerComponent},
+  {path : 'of',component:OfComponent}
 ];
---
+++ src/app/of/of.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { of, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-of',
+  standalone: true,
+  imports: [],
+  templateUrl: './of.component.html',
+  styleUrl: './of.component.scss'
+})
+export class OfComponent {
+  ofsubscription:Subscription | undefined;
+
+  constructor()
+  {
+    // this.ofsubscription = of(1,2,3,4,5,6,7,8,9).subscribe((arg:any)=>{
+    //   console.log(arg);
+    // })
+
+  }
+
+  ngOnInit(): void {
+    this.ofsubscription = of('angular','react','json','python').subscribe((res:any)=>{
+      console.log(res);
+      let ofc = document.createElement('li');
+      ofc.innerHTML = res;
+      document.getElementById('hellojs')?.appendChild(ofc)
+    })
+  }
+
+
+}
