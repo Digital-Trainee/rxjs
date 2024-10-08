cb9fe260f118a5924c6b1ef8cc0e6efe3a661904
Digital-Trainee <digitaltraineepune@gmail.com>
2024-10-08 17:29:51 +0530

    create time componet set route and timer use with Subscription

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
+import { Component } from '@angular/core';
 import { IntervalComponent } from './interval/interval.component';
 import { Routes } from '@angular/router';
+import { TimerComponent } from './timer/timer.component';
 
 export const routes: Routes = [
-  {path:'',component: IntervalComponent }
+  {path:'',component: IntervalComponent },
+  {path:'timer',component:TimerComponent}
 ];
---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+
+@Component({
+  selector: 'app-timer',
+  standalone: true,
+  imports: [],
+  templateUrl: './timer.component.html',
+  styleUrl: './timer.component.scss'
+})
+export class TimerComponent {
+  timers = timer(5000,1000);
+
+  timerSubscriptions: Subscription | undefined;
+  constructor()
+  {
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    this.timers.subscribe((Res:any)=>{
+      console.log(Res);
+      let pages  = <any>document.createElement('p');
+      pages.innerHTML = Res;
+      document.getElementById('page')?.append(pages);
+    })
+  }
+
+}
