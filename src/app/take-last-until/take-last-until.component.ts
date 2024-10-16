457cfb2cc91428332472bf90464dd6c673092933
suhas-borkar <digitaltraineepune@gmail.com>
2024-10-16 18:50:26 +0530

    try takelast example and set commne

--- src/app/take-last-until/take-last-until.component.ts
+++ src/app/take-last-until/take-last-until.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { from, take } from 'rxjs';
+import { from, take, takeLast } from 'rxjs';
 
 @Component({
   selector: 'app-take-last-until',
@@ -12,8 +12,15 @@ export class TakeLastUntilComponent implements OnInit {
   user:any = ['abcd','xyz','pqr','lmn','erw','synt','dj','babu','rao'];
 
   ngOnInit(): void {
+    // here we are use take operator and take first five streams
     from(this.user).pipe(take(5)).subscribe((res:any)=>{
-      console.log(res)
+      console.log('take:- '+ res)
+    })
+
+
+    // here takelast is focus on last  log/stream
+    from(this.user).pipe(takeLast(5)).subscribe((res:any)=>{
+      console.log( 'takelast:- '+res);
     })
   }
 
