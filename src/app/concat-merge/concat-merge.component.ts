b6b5b6740ead552376267a329c06ba2ee440cd26
suhas-borkar <digitaltraineepune@gmail.com>
2024-11-08 18:41:11 +0530

    create concat and merge component

---
+++ src/app/concat-merge/concat-merge.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-concat-merge',
+  standalone: true,
+  imports: [],
+  templateUrl: './concat-merge.component.html',
+  styleUrl: './concat-merge.component.scss'
+})
+export class ConcatMergeComponent {
+
+}
