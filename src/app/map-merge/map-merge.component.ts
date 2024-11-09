8364e056dec459bb7b8f75351ad6c0637dfac419
suhas-borkar <digitaltraineepune@gmail.com>
2024-11-09 18:30:47 +0530

    Here We can try map , map + margeAll and mergeMap for inner subscribe

--- src/app/map-merge/map-merge.component.ts
+++ src/app/map-merge/map-merge.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, map, mergeAll, mergeMap, of } from 'rxjs';
 
 @Component({
   selector: 'app-map-merge',
@@ -7,6 +8,37 @@ import { Component } from '@angular/core';
   templateUrl: './map-merge.component.html',
   styleUrl: './map-merge.component.scss'
 })
-export class MapMergeComponent {
+export class MapMergeComponent implements OnInit {
 
+  ngOnInit(): void {
+    const users = from(['suhas','abcd','xyz','pqr','yzw']);
+
+    // example 01 :- map for multiple subscribe
+    // here we are only use map operator for multisubscription
+    users.pipe(map(res=> this.getdata(res))).subscribe((res:any)=>{
+      console.log(res); // here we are get a observable strem
+      res.subscribe((res2:any)=>{
+        console.log(res2); //  here we get data from getdata(user) function
+      })
+    })
+
+
+    // example 02 :- map + margeAll
+    // here we are use of merge all operator for skip inner subscribe.
+    users.pipe(map(res => this.getdata(res)),mergeAll()).subscribe((res:any)=>{
+      console.log(res); // Here, we obtain direct value from the getdata() function.
+    })
+
+    console.clear()
+    // example 03 :- margeMap
+    //  here we go and we get direct inner data of getdata function which we call in mergeMap operator
+    users.pipe(mergeMap(res=> this.getdata(res))).subscribe((Res:any)=>{
+      console.log(Res); //  this is a best way for get data using mergeMap
+    })
+  }
+
+  getdata(user:any)
+  {
+    return of(`return data of ${user}`);
+  }
 }
