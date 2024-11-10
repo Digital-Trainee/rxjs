990c4adbb0aec6d7f355385abfdf4fb8bace2ed2
suhas-borkar <digitaltraineepune@gmail.com>
2024-11-10 19:16:04 +0530

    crete switchmapExample component for try some api in this

---
+++ src/app/switch-map-example/switch-map-example.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-map-example',
+  standalone: true,
+  imports: [],
+  templateUrl: './switch-map-example.component.html',
+  styleUrl: './switch-map-example.component.scss'
+})
+export class SwitchMapExampleComponent {
+
+}
